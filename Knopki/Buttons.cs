051764b60a76601b60ad29c76b13adce4a43290e
олег <@DESKTOP-SSTKF2A>
2023-03-18 20:36:41 +0200

    Добавьте файлы проекта.

---
+++ Knopki/Buttons.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Knopki
+{
+    public abstract class Button
+    {
+        public abstract void Draw();
+    }
+
+    // Клас кнопки звичайного типу
+    public class NormalButton : Button
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Normal Button");
+        }
+    }
+
+    // Клас кнопки RadioButton
+    public class RadioButton : Button
+    {
+        public override void Draw()
+        {
+            MessageBox.Show("Radio Button");
+        }
+    }
+
+    // Клас кнопки CheckButton
+    public class CheckButton : Button
+    {
+        public override void Draw()
+        {
+            MessageBox.Show("Check Button");
+        }
+    }
+
+    // Абстрактний клас фабрики кнопок
+    public abstract class ButtonFactory
+    {
+        public abstract Button CreateButton();
+    }
+
+    // Фабрика для створення звичайної кнопки
+    public class NormalButtonFactory : ButtonFactory
+    {
+        public override Button CreateButton()
+        {
+            return new NormalButton();
+        }
+    }
+
+    // Фабрика для створення RadioButton
+    public class RadioButtonFactory : ButtonFactory
+    {
+        public override Button CreateButton()
+        {
+            return new RadioButton();
+        }
+    }
+
+    // Фабрика для створення CheckButton
+    public class CheckButtonFactory : ButtonFactory
+    {
+        public override Button CreateButton()
+        {
+            return new CheckButton();
+        }
+    }
+}
+
+   
---
+++ Knopki/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+
+namespace Knopki
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ButtonFactory normalButtonFactory = new NormalButtonFactory();
+            ButtonFactory radioButtonFactory = new RadioButtonFactory();
+            ButtonFactory checkButtonFactory = new CheckButtonFactory();
+
+            // Створення звичайної кнопки
+            Button normalButton = normalButtonFactory.CreateButton();
+            normalButton.Draw();
+
+            // Створення RadioButton
+            Button radioButton = radioButtonFactory.CreateButton();
+            radioButton.Draw();
+
+            // Створення CheckButton
+            Button checkButton = checkButtonFactory.CreateButton();
+            checkButton.Draw();
+
+          
+        }
+    }
+
+}
